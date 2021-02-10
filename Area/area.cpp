938c0dab9b42f23a3442c989d3624154e9bf64b1
wes-public-apps <murraywj@yahoo.com>
2021-02-09 18:45:16 -0800

    use commandline args to control what is run

--- Area/area.cpp
+++ Area/area.cpp
@@ -6,7 +6,7 @@
 
 //compiler directives to import libraries
 #include <iostream>
-#include <string>
+#include <cstring>
 #include <cmath>
 
 using namespace std;
@@ -44,9 +44,30 @@ float circleArea(){
     return area;
 }
 
+//define list of possible args
+string argList(){
+    return "\
+    --rectangle   calculates the area of a rectangle. \n\
+    --rect        calculates the area of a rectangle. \n\
+    --circle      calculates the area of a circle. \n\
+    --help        list possible args. \n";
+}
+
 //define run method
-int main(int numArgs, char **args){
-    rectArea();
-    circleArea();
+int main(int numArgs, char** args){
+
+    // for each arg run associated code
+    for(int i = 1; i < numArgs; i++){
+        if(!strcmp(args[i],"--rectangle")){
+            rectArea();
+        } else if (!strcmp(args[i],"--circle")){
+            circleArea();
+        } else if (!strcmp(args[i],"--help")){
+            cout << argList();
+        } else {
+            cout << args[i] << " is not a valid input." << endl;
+        }
+    }
+
     return 0;
 }
